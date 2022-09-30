6850031df9de4af4fb01a6227049af6808f845a1
baberarjumand <baber.arjumand@gmail.com>
2022-09-30 03:22:54 -0700

    add cloud func to get connected wallets for user

--- cloud/index.js
+++ cloud/index.js
@@ -1,3 +1,47 @@
 Moralis.Cloud.define("helloWorld", (request) => {
   return "Hello World";
 });
+
+Moralis.Cloud.define(
+  "getUsersConnectedWallets",
+  async (request) => {
+    const logger = Moralis.Cloud.getLogger();
+
+    try {
+      const userHandle = request.params.userHandle;
+
+      // get user handle object for corresponding userHandle
+      const query = new Moralis.Query("UserHandle");
+      query.equalTo("handle", userHandle);
+      const userHandleObjs = await query.find({ useMasterKey: true });
+      
+    //   logger.info("Query Result: ", userHandleObjs);
+    //   return userHandleObjs;
+
+    //   return userHandleObjs[0].get("connectedUsers");
+
+      const connectedUsers = userHandleObjs[0].get("connectedUsers");
+      const connectedWalletsArr = [];
+
+      for (let userObj of connectedUsers) {
+        if (!userObj.isDataAvailable()) {
+          await userObj.fetch({ useMasterKey: true });
+        }
+
+        if (userObj.has("accounts")) {
+          connectedWalletsArr.push(userObj.get("accounts")[0]);
+        }
+      }
+
+      return [...new Set(connectedWalletsArr)];
+    } catch (err) {
+      logger.error("Error in getUsersConnectedWallets!");
+      logger.error(err);
+      throw err;
+    }
+  },
+  {
+    fields: ["userHandle"],
+    requireUser: true,
+  }
+);
