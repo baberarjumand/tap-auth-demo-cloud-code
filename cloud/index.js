d118ff3a6226f05fb306a244605a5a10fca11d8e
baberarjumand <baber.arjumand@gmail.com>
2022-09-30 07:35:33 -0700

    updated connected wallets logic and added connected emails logic

--- cloud/index.js
+++ cloud/index.js
@@ -5,7 +5,7 @@ Moralis.Cloud.define("helloWorld", (request) => {
 Moralis.Cloud.define(
   "getUsersConnectedWallets",
   async (request) => {
-    const logger = Moralis.Cloud.getLogger();
+    // const logger = Moralis.Cloud.getLogger();
 
     try {
       const userHandle = request.params.userHandle;
@@ -20,7 +20,7 @@ Moralis.Cloud.define(
 
       //   return userHandleObjs[0].get("connectedUsers");
 
-      const connectedUsers = userHandleObjs[0].get("connectedUsers");
+      const connectedWalletUsers = userHandleObjs[0].get("connectedWalletUsers");
       const connectedWalletsArr = [];
 
       // iterate through user objects
@@ -34,18 +34,22 @@ Moralis.Cloud.define(
       //     }
       //   }
 
-      // iterate through user ids
-      for (let userId of connectedUsers) {
-        const userObj = new Moralis.User();
-        userObj.id = userId;
-        await userObj.fetch({ useMasterKey: true });
-
-        if (userObj.has("accounts")) {
-          connectedWalletsArr.push(userObj.get("accounts")[0]);
-        }
+      if (connectedWalletUsers.length > 0) {
+          // iterate through user ids
+          for (let userId of connectedWalletUsers) {
+            const userObj = new Moralis.User();
+            userObj.id = userId;
+            await userObj.fetch({ useMasterKey: true });
+    
+            if (userObj.has("accounts")) {
+              connectedWalletsArr.push(userObj.get("accounts")[0]);
+            }
+          }
+    
+          return [...new Set(connectedWalletsArr)];
+      } else {
+        return [];
       }
-
-      return [...new Set(connectedWalletsArr)];
     } catch (err) {
       logger.error("Error in getUsersConnectedWallets!");
       logger.error(err);
@@ -57,3 +61,63 @@ Moralis.Cloud.define(
     requireUser: true,
   }
 );
+
+Moralis.Cloud.define(
+    "getUsersConnectedEmails",
+    async (request) => {
+      const logger = Moralis.Cloud.getLogger();
+  
+      try {
+        const userHandle = request.params.userHandle;
+  
+        // get user handle object for corresponding userHandle
+        const query = new Moralis.Query("UserHandle");
+        query.equalTo("handle", userHandle);
+        const userHandleObjs = await query.find({ useMasterKey: true });
+  
+        //   logger.info("Query Result: ", userHandleObjs);
+        //   return userHandleObjs;
+  
+        //   return userHandleObjs[0].get("connectedUsers");
+  
+        const connectedEmailUsers = userHandleObjs[0].get("connectedEmailUsers");
+        const connectedEmailsArr = [];
+  
+        // iterate through user objects
+        //   for (let userObj of connectedUsers) {
+        //     if (!userObj.isDataAvailable()) {
+        //       await userObj.fetch({ useMasterKey: true });
+        //     }
+  
+        //     if (userObj.has("accounts")) {
+        //       connectedWalletsArr.push(userObj.get("accounts")[0]);
+        //     }
+        //   }
+  
+        if (connectedEmailUsers.length > 0) {
+            // iterate through user ids
+            for (let userId of connectedEmailUsers) {
+              const userObj = new Moralis.User();
+              userObj.id = userId;
+              await userObj.fetch({ useMasterKey: true });
+      
+              if (userObj.has("email")) {
+                connectedEmailsArr.push(userObj.get("email"));
+              }
+            }
+      
+            return [...new Set(connectedEmailsArr)];
+        } else {
+          return [];
+        }
+      } catch (err) {
+        logger.error("Error in getUsersConnectedEmails!");
+        logger.error(err);
+        throw err;
+      }
+    },
+    {
+      fields: ["userHandle"],
+      requireUser: true,
+    }
+  );
